f4609d6fd0c47965d360d66743787a886d23cadf
shovon <shovon@gmail.com>
2025-01-10 10:04:50 +0600

    Complete Ecommerce Pizza Web-App

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -9,7 +9,7 @@ export default function Button({ children, disabled, to, type, onClick }) {
     small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
     round: base + " px-3 py-2 md:px-3 md:py-2 text-xs",
     secondary:
-      "inline-block text-sm rounded-full border-1 font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 focus:bg-stone-300 focus:outline-none focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-3 md:px-6 md:py-4",
+      "inline-block text-sm rounded-full border-2 font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 focus:bg-stone-300 focus:outline-none focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-3 md:px-6 md:py-4",
   };
 
   if (to)
